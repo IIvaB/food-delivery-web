162f2844497b9b8b790fdbf588fdacdb6315489f
IIvaB <ivanbudniakapple@gmail.com>
2026-07-10 21:04:49 +0300

    Adapt product service to JPA repository

--- src/main/java/com/fd/app/service/ProductService.java
+++ src/main/java/com/fd/app/service/ProductService.java
@@ -2,9 +2,12 @@ package com.fd.app.service;
 
 import com.fd.app.repository.ProductRepository;
 import com.fd.app.entity.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class ProductService{
     private final ProductRepository productRepository;
 
@@ -12,39 +15,33 @@ public class ProductService{
         this.productRepository = productRepository;
     }
 
-    public void addProduct(Product product) {
-        if (product == null) {
-            throw new IllegalArgumentException("Product cannot be null");
-        }
-        if (productRepository.existsProductById(product.getId())) {
-            throw new IllegalArgumentException("Product already exists");
-        }
-        productRepository.addProductMap(product);
+    public Product addProduct(Product product) {
+        productRepository.save(product);
+        return product;
     }
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
-
-    public Product getProductById(Long id){
+    public Optional<Product> getProductById(Long id){
         if(id == null){
             throw new IllegalArgumentException("Product id cannot be null");
         }
-        if(!productRepository.existsProductById(id)){
+
+        Optional<Product> product = productRepository.findById(id);
+
+        if(product == null){
             throw new IllegalArgumentException("Product not found");
         }
-        Product product = productRepository.getProductById(id);
+
         return product;
     }
 
-    public void removeProductById(Long id){
+    public void deleteProductById(Long id){
         if(id == null){
             throw new IllegalArgumentException("Product id cannot be null");
         }
-        if(!productRepository.existsProductById(id)){
-            throw new IllegalArgumentException("Product doesn't exist");
-        }
-        productRepository.removeProductById(id);
+        productRepository.deleteById(id);
     }
 }
