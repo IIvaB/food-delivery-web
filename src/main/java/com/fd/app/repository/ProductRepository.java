6fed665638eaee1d6126d3aa4a519d9446010c36
IIvaB <ivanbudniakapple@gmail.com>
2026-07-10 20:32:09 +0300

    Convert product repository to Spring Data JPA

--- src/main/java/com/fd/app/repository/ProductRepository.java
+++ src/main/java/com/fd/app/repository/ProductRepository.java
@@ -1,33 +1,7 @@
 package com.fd.app.repository;
 
 import com.fd.app.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class ProductRepository {
-
-    private final Map<Long, Product> productMap = new HashMap<>();
-
-    public void addProductMap(Product product) {
-        productMap.put(product.getId(), product);
-    }
-
-    public Product getProductById(Long id) {
-        return productMap.get(id);
-    }
-
-    public  boolean existsProductById(Long id) {
-        return productMap.containsKey(id);
-    }
-
-    public List<Product> findAll() {
-        return new ArrayList<>(productMap.values());
-    }
-
-    public void removeProductById(Long id) {
-        productMap.remove(id);
-    }
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
