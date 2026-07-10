f4f3c8e11a4aa84ac4bccac5840d2acffe20622c
IIvaB <ivanbudniakapple@gmail.com>
2026-07-10 20:01:11 +0300

    Add Spring Boot application entry point

---
+++ src/main/java/com/fd/app/FoodDeliveryApplication.java
@@ -0,0 +1,11 @@
+package com.fd.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//точка входу для Spring
+@SpringBootApplication
+public class FoodDeliveryApplication {
+    public static void main(String[] args) {
+
+    }
+}
